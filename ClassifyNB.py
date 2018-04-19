8479ead376af55eb16f2d1773c7946914fc40a63
Rodrigo Gutierrez <r.gutierrez@dverse.me>
2018-04-19 19:29:48 +0900

    Add timer and SVM case.

--- ClassifyNB.py
+++ ClassifyNB.py
@@ -1,4 +1,6 @@
-import sklearn.naive_bayes as sk
+#import sklearn.naive_bayes as sk
+import sklearn.svm as sk
+from time import time
 
 def classify(features_train, labels_train):   
     ### import the sklearn module for GaussianNB
@@ -6,9 +8,18 @@ def classify(features_train, labels_train):
     ### fit the classifier on the training features and labels
     ### return the fit classifier
     
-    clf = sk.GaussianNB()
+    #clf = sk.GaussianNB()
+    clf = sk.SVC(kernel='rbf', C=100000.0, gamma=10.0)
+
+    print "Training start"
+    timer = time()
     clf.fit(features_train, labels_train)
-    #pred = clf.predict(features_test)
+    print "Training time: ", round(time() - timer, 3), "s"
+
+    #print "Predicting start"
+    #timer = time()
+    #clf.predict(features_test)
+    #print "Predicting time: ", round(time() - timer, 3), "s"
 
     #accuracy = clf.score(features_test, labels_test)
     #print(accuracy)
